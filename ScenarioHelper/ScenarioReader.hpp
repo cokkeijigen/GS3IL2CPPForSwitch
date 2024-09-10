23f7510bb1952c01aa165cfebda3dffa8fe1acd0
cokkeijigen <cokkeijigen@qq.com>
2024-09-10 09:01:14 +0800

    Update ScenarioReader.hpp

--- ScenarioHelper/ScenarioReader.hpp
+++ ScenarioHelper/ScenarioReader.hpp
@@ -15,7 +15,7 @@ namespace ScenarioSystem {
 		int32_t StringTblOfs;
 		struct Token {
 			uint8_t Type, Id, v1, v2;
-			inline auto Arg() -> uint16_t&& { return v2 | (v1 << 8); }
+			inline auto Arg() -> uint16_t&& { return (v1 << 8) | v2; }
 			inline auto Arg(uint16_t arg)   { v1 = uint8_t(arg >> 8), v2 = uint8_t(arg & 0x00FF); }
 		} TokenTbl[0];
 	};
